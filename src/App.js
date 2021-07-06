fbd2dc6b211840413d2a06b1d26f4dc9d755a7df
sulemanbutta <sulemanbutta98@gmail.com>
2021-07-06 18:14:58 -0500

    t

--- src/App.js
+++ src/App.js
@@ -1,59 +1,16 @@
 import React from "react";
-
 // Component imports
+import Results from './components/Results'
 import Header from './components/Header'
 import StartSim from './components/StartSim'
-import LinearProgress from '@material-ui/core/LinearProgress'
 
-export default class App extends React.Component {
-  state = {
-    loading: true,
-    player1name: null,
-    player2name: null,
-    results: []
-  };
-  async componentDidMount() {
-    //const url = "http://localhost:4004/sim/1/2/11";
-    const url = "https://bball-1v1.herokuapp.com/sim/1/2/11";
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    this.setState({ player1name: data.player1.name});
-    this.setState({ player2name: data.player2.name});
-    this.setState({ results: data.turns, loading: false });
-  }
-  render() {
-    if (this.state.loading) {
-      return (
-        <div>
-          <Header />
-          <LinearProgress style={{
-            marginTop: '2rem',
-            backgroundColor: '#32373B'
-          }} />
-        </div>)
-    }
-    if (!this.state.results.length) {
-      return (
-        <div >
-          <Header />
-          <h1>No results.</h1>
-        </div>);
-    }
-    return (
-      <div style={{
-        backgroundColor: '#32373B',
-        color: '#F4D6CC'
-      }}>
-        <Header />
-        <StartSim />
-        {this.state.results.map(eachTurn => (
-          <div key={eachTurn.turn}>
-            <div>Possession: {eachTurn.turn}<br></br>{this.state.player1name}: {eachTurn.p1Score}<br></br>{this.state.player2name}: {eachTurn.p2Score}</div>
-            <br></br>
-          </div>
-        ))}
-      </div>
-    );
-  }
+function App() {
+	return (
+    <div>
+			<Header />
+      <StartSim/>
+      <Results />
+    </div>
+	);
 }
+export default App;
