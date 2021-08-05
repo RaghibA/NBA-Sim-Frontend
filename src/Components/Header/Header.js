9ecdd1e3eb268a386766abc55e58f262fa2eff08
Raghib Aftab <raghib@raghibs-air.lan>
2021-08-05 04:24:17 -0500

    Header component done, needs link to about section

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,14 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-      <h1>NBA 1v1</h1>
+      <div className='nav'>
+        <div className='nav-logo'>
+          <h1 style={{fontSize: '18px'}}>NBA 1V1</h1>
+        </div>
+        <div className='nav-links'>
+          <a href='' className='nav-link'>About</a>
+        </div>
+      </div>
     </div>
   )
 }
