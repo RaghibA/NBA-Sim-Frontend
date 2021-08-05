19e2b51a23e6a4a740129e2343a67b8487a40769
Raghib Aftab <raghib@raghibs-air.lan>
2021-08-05 15:29:52 -0500

    card image and text

--- src/Components/Splash/Splash.js
+++ src/Components/Splash/Splash.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import img1 from '../../assets/img1.png'
 
 // Styles Import
 import './Splash.css'
@@ -14,12 +15,24 @@ const Splash = () => {
         </div>
         <div className='splash-section splash-1'>
           <h2 className='text-primary'>Pick Your Players</h2>
+          <div>
+            <img className='splash-img' src={img1} />
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate sequi dolorum laboriosam.</p>
         </div>
         <div className='splash-section splash-2'>
           <h2 className='text-primary'>Set The Score</h2>
+          <div>
+            <img className='splash-img' src={img1} />
+          </div>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className='splash-section splash-2'>
           <h2 className='text-primary'>Start The Game</h2>
+          <div>
+            <img className='splash-img' src={img1} />
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, iste. Repellendus!</p>
         </div>
       </div>
     </div>
