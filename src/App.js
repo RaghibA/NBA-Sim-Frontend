7319b42136ed5cc346f2cb69e0a43b33a43c26e2
Raghib Aftab <raghib@raghibs-air.lan>
2021-08-07 03:44:43 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import React, {useState} from 'react';
 // Component Imports
 import Header from './Components/Header/Header'
 import Splash from './Components/Splash/Splash'
-import PlayerSelect from './Components/PlayerSelect/PlayerSelect'
 import Simulation from './Components/Simulation/Simulation'
 import About from './Components/About/About'
 
@@ -14,24 +13,10 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 const App = () => {
 
-  const [simData, setSimData] = useState({
-    player1: 1,
-    player2: 2,
-    scoreLimit: 11,
-    sim: false
-  })
-
-  const simDataHandler = (newSimData) => {
-    setSimData(newSimData)
-
-    console.log(simData);
-  }
-
   return (
     <div className="App">
       <Header />
       <Splash />
-      <PlayerSelect onSaveSimData={simDataHandler}/>
       <Simulation />
       <About />
     </div>
